090e8d07c0a8972d677eba71a65487024986b9c0
Muraino Yakubu <murainoy@yahoo.com>
2020-06-14 10:39:00 +0100

    pass data to movies details

--- lib/src/utils/url_constant.dart
+++ lib/src/utils/url_constant.dart
@@ -2,7 +2,7 @@ class UrlConstant {
   static const String BASE_URL =
       "http://api.themoviedb.org/3/movie";
 
-  static const String API_KEY = 'enter key';
+  static const String API_KEY = 'enter keymo';
 
   static const String TAG = "details";
 }
